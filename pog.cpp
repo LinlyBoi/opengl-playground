2283cfef096c3dc9de833ac5ec16695a019483d9
LinlyBoi <linlysmolworthy@gmail.com>
2024-08-02 16:07:10 +0300

    incompatibility moment

--- pog.cpp
+++ pog.cpp
@@ -49,7 +49,7 @@ int main() {
   // compiling the shaders
   const char *vertexShaderSource =
       "#version 330 core\n"
-      "layout (location = 0) in vec3 aPos;\n"
+      "in vec3 aPos;\n"
       "void main()\n"
       "{\n"
       " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
@@ -73,7 +73,7 @@ int main() {
     std::cout << "Vertex shader is gOOd!" << std::endl;
   const char *fragmentShaderSource =
       "#version 330 core\n"
-      "layout (location = 0) in vec3 aPos;\n"
+      "in vec3 aPos;\n"
       "out vec4 FragColor;\n"
       "void main()\n"
       "{\n"
