8544a3277ee5ec2cefca7e36bea2a410184f9448
LinlyBoi <linlysmolworthy@gmail.com>
2024-08-02 14:00:33 +0300

    It's friday....

--- Makefile
+++ Makefile
@@ -3,11 +3,25 @@
 #
 # @file
 # @version 0.1
+CC = g++
+CFLAGS = -Wall
+LIBS = -lglfw -lGLESv2 -lm -lGL -lGLEW -ldl
+INCL = ./include
+DEPS =  gl_utils.cpp glad.c
+EXE = pog
 
-build:
-	g++ -o ./out/pog pog.cpp -lglfw -lGLESv2 -lm -lGL
+
+all: build run
+
+build: $(EXE).cpp $(DEPS)
+	$(CC) $(CFLAGS) $^ $(LIBS) -I$(INCL) -o ./out/$(EXE)
+
+debug: $(EXE).cpp $(DEPS)
+	$(CC) -g $(CFLAGS) $^ $(LIBS) -I$(INCL) -o ./out/$(EXE)-debug
+
 run: build
 	./out/pog
 
-
+gdb: debug
+	gdb ./out/debug
 # end
--- pog.cpp
+++ pog.cpp
@@ -1,12 +1,12 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <EGL/egl.h>
+// #include <GL/glew.h>
+#include "include/gl_utils.h"
 #include <GL/glut.h>
+#include <GLFW/glfw3.h>
 // #include <GLES2/gl2.h>
-#include <iostream>
 #include "pog.h"
+#include <iostream>
 int main() {
-  int x;
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -23,28 +23,22 @@ int main() {
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  while (!glfwWindowShouldClose(window)) {
-    processInput(window);
-
-    glClearColor(0.2f, 0.3f, 0.6f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
   }
-  float vertices[] = {
-  -0.5f, -0.5f, 0.0f,
-  0.5f, -0.5f, 0.0f,
-  0.0f, 0.5f, 0.0f
-  };
+  if (!GLAD_GL_ARB_direct_state_access) {
+    /* see
+     * https://www.khronos.org/registry/OpenGL/extensions/ARB/ARB_direct_state_access.txt
+     * https://www.khronos.org/opengl/wiki/Direct_State_Access. This is the way.
+     */
+    std::cout << "DSA not supported!" << std::endl;
+    return -1;
+  }
 
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-  //compiling the shaders
+  // compiling the shaders
   const char *vertexShaderSource =
       "#version 330 core\n"
       "layout (location = 0) in vec3 aPos;\n"
@@ -53,7 +47,7 @@ int main() {
       " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
       "}\0";
   unsigned int vertexShader;
-  vertexShader = glCreateShader(GL_VERTEX_SHADER); //compile code
+  vertexShader = glCreateShader(GL_VERTEX_SHADER); // compile code
 
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
   glCompileShader(vertexShader);
@@ -66,7 +60,7 @@ int main() {
     glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
               << infoLog << std::endl;
-  } //check if compilation succesful
+  } // check if compilation succesful
   const char *fragmentShaderSource =
       "#version 330 core\n"
       "layout (location = 0) in vec3 aPos;\n"
@@ -78,42 +72,73 @@ int main() {
   unsigned int fragmentShader;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader); //compile fragment shader
+  glCompileShader(fragmentShader); // compile fragment shader
 
   unsigned int shaderProgram;
-  shaderProgram = glCreateProgram(); //create shader program
+  shaderProgram = glCreateProgram(); // create shader program
 
-  //attaching shaders to our program
+  // attaching shaders to our program
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
 
-  glUseProgram(shaderProgram); //use it NOW
+  glUseProgram(shaderProgram); // use it NOW
 
-  //not needed anymore they're linked
+  // not needed anymore they're linked
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  //tell OpenGL's stupid existence how to interpret data
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
+  // Vertex data
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
 
-  // 0. copy our vertices array in a buffer for OpenGL to use
+  unsigned int VBO, VAO;
+  glGenBuffers(1, &VBO);
+  // bind vertex object
+  glGenVertexArrays(1, &VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  // 1. then set the vertex attributes pointers
+
+  // tell OpenGL's stupid existence how to interpret data
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
-  // 2. use our shader program when we want to render an object
-  glUseProgram(shaderProgram);
-  // 3. now draw the object
 
-  unsigned int VAO;
-  glGenVertexArrays(1, &VAO);
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    // -----
+    processInput(window);
 
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw our first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(
+        VAO); // seeing as we only have a single VAO there's no need to bind it
+              // every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(0); // no need to unbind it every time
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
   glfwTerminate();
   return 0;
 }
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
 }
